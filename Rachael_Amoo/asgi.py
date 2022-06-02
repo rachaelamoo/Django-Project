be4bb31ebf0df0862dc8e811fc81c08830cc6fac
Rachael3 <amoorachael3@gmail.com>
2022-06-02 01:51:44 +0100

    First commit

---
+++ Rachael_Amoo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Rachael_Amoo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rachael_Amoo.settings')
+
+application = get_asgi_application()
